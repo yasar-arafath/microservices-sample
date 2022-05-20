62fe6ba4c32a60b0d4ec67c539163d0be0027294
Yasar arafat <yasararafate@gmail.com>
2022-05-20 20:13:36 +0530

    Add files via upload

---
+++ service-two/src/main/java/com/mudigal/two/model/NameValueTO.java
@@ -0,0 +1,16 @@
+package com.mudigal.two.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author Yasar Arafat
+ */
+@Data
+@ToString
+public class NameValueTO {
+
+  private String name;
+  private String value;
+
+}
